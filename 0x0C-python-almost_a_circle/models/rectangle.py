8468203bf0db57270edfad1fd49e7580a780012e
root <vincent.okoth856@gmail.com>
2024-01-17 10:32:04 -0800

    A class Rectangle that inherits from Base.

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# rectangle.py
+"""Defines a rectangle class."""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Represents a rectangle."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new rectangle.
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
