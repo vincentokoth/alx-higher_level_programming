e7482fcade3f0f9f5e349ded9176f4d3e79dd812
root <vincent.okoth856@gmail.com>
2024-02-27 18:50:54 -0800

    Updated program to cater for negative digits.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,8 @@ import random
 number = random.randint(-10000, 10000)
 
 if number < 0:
-    lastDigit = -(-number % 10) # Ensure the last digit is negative for negative numbers
+    lastDigit = -(-number % 10)  """ Ensure the last digit is negative
+                                     for negative numbers """
 else:
     lastDigit = number % 10
 
